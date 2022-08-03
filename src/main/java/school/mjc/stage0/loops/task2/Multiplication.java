f4364949b7257893a78939e7af75a803fc6ea605
Ziyosh <ziyodaakhmedova1998@gmail.com>
2022-08-03 12:23:04 +0300

    Loops task2 commit

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+        int i = 0;
+        boolean pos = true;
+
+        if(multiplyByAndToInclusive>0) {
+            while (i <= multiplyByAndToInclusive) {
+                System.out.println(i * multiplyByAndToInclusive);
+                i++;
+            }
+        }else{
+
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
+        int base = 2;
+        int result = 1;
+        int i = 1;
 
+        if(power>=0) {
+            System.out.println(i);
+            while (i <= power) {
+                result *= base;
+                System.out.println(result);
+                i++;
+            }
+        }else{
+            System.out.println("too much power");
+        }
     }
 }
