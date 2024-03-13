fd66852d32a480837846c34fe55a6047634dbe5b
Ziyosh <ziyodaakhmedova1998@gmail.com>
2024-03-13 16:35:08 +0500

    x

--- src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
@@ -3,11 +3,10 @@ package school.mjc.stage0.loops.task2;
 public class EvenNumbersPrinter {
     public void printEvenNumbers(int printTillInclusive) {
 
-        int i = 1;
-        while(i <= printTillInclusive){
-            if( i%2 == 0)
-                System.out.println(i);
-            i++;
+        int i = 2;
+        while(i<=printTillInclusive){
+            System.out.println(i);
+            i += 2;
         }
     }
 }
